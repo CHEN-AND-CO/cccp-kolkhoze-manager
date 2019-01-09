d823468779c02a7043e710f10e7d6458b8da460e
HerrCraziDev <herrcrazi@gmail.com>
2019-01-09 17:49:30 +0100

    Created Angular routes, fixed CSS text alignment with symbol fonts

--- angular/cccpkm-front/src/app/app-routing.module.ts
+++ angular/cccpkm-front/src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppHomeComponent } from './app-home/app-home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: AppHomeComponent },
+  // { path: 'u/:uname/trips/:tid/edit', component: /*TODO*/ },
+  // { path: 'u/:uname/trips', component: /*TODO*/ },
+  // { path: 'u/:uname/new', component: /*TODO*/ },
+  // { path: 'u/:uname/edit', component: /*TODO*/ },
+  // { path: 'u/:uname', component: /*TODO*/ },
+  { path: '', redirectTo: '/home' },
+  // { path: '**', component: /*TODO*/ }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
