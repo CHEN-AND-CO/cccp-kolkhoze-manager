73160213970d6c0f29f0a7d097443e245d189f99
HerrCraziDev <herrcrazi@gmail.com>
2019-01-10 02:53:13 +0100

    Added routes and modules

--- angular/cccpkm-front/src/app/app-routing.module.ts
+++ angular/cccpkm-front/src/app/app-routing.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
 import { AppHomeComponent } from './app-home/app-home.component';
+import { TripListComponent } from './trip-list/trip-list.component';
+import { TripEditComponent } from './trip-edit/trip-edit.component';
+import { UserEditComponent } from './user-edit/user-edit.component';
+import { ErrorComponent } from './error/error.component';
 
 const routes: Routes = [
   { path: 'home', component: AppHomeComponent },
-  // { path: 'u/:uname/trips/:tid/edit', component: /*TODO*/ },
-  // { path: 'u/:uname/trips', component: /*TODO*/ },
-  // { path: 'u/:uname/new', component: /*TODO*/ },
-  // { path: 'u/:uname/edit', component: /*TODO*/ },
-  // { path: 'u/:uname', component: /*TODO*/ },
-  { path: '', redirectTo: '/home' },
-  // { path: '**', component: /*TODO*/ }
+  { path: 'u/:uname/trips/:tid/edit', component: TripEditComponent },
+  { path: 'u/:uname/trips', component: TripListComponent },
+  { path: 'u/:uname/new', component: TripEditComponent },
+  { path: 'u/:uname/edit', component: UserEditComponent },
+  { path: 'u/:uname', component: AppHomeComponent },
+  { path: '', component: AppHomeComponent },
+  { path: '**', component: ErrorComponent }
 ];
 
 @NgModule({
--- angular/cccpkm-front/src/app/app.module.ts
+++ angular/cccpkm-front/src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppHomeComponent } from './app-home/app-home.component';
+import { TripListComponent } from './trip-list/trip-list.component';
+import { TripEditComponent } from './trip-edit/trip-edit.component';
+import { UserEditComponent } from './user-edit/user-edit.component';
+import { ErrorComponent } from './error/error.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AppHomeComponent
+    AppHomeComponent,
+    TripListComponent,
+    TripEditComponent,
+    UserEditComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
