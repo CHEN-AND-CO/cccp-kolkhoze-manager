c8f88079a6ad8b75f3e682487d40bbd0149ff8fe
HerrCraziDev <herrcrazi@gmail.com>
2019-01-11 14:25:23 +0100

    Trip list requests

--- angular/cccpkm-front/src/app/trip-list/trip-list.component.ts
+++ angular/cccpkm-front/src/app/trip-list/trip-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ActivatedRouteSnapshot } from '@angular/router';
 import { ApiClient, Trip, User } from '../../lib/api-client';
 
 @Component({
@@ -10,12 +11,28 @@ import { ApiClient, Trip, User } from '../../lib/api-client';
 export class TripListComponent implements OnInit {
 
   trips: Trip[];
+  user: User;
+  uid: any;
 
-  constructor(private api: ApiClient) { }
+  constructor(private api: ApiClient, private route: ActivatedRoute) { }
+  
+  async ngOnInit() {
+    console.log("Aloha");
 
-  ngOnInit() {
+    // this.route.paramMap.subscribe( params => {
+    //   this.uid = parseInt( params.get('uid') );
+    //   console.log(this.uid);
+    // });
+
+    this.uid = this.route.snapshot.params.uid;
+    console.log(this.uid);
+    console.log("Fini")
+
     this.api.getTrips()
             .subscribe( (data: Trip[]) => this.trips = { ...data });
+
+    this.api.getUser(this.uid)
+            .subscribe( (data: User) => this.user = { ...data });
   }
 
 }
